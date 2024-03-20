70654b1ca8254dc1864620174e3e09445b950d68
Paulo Honorato <paulohhonoratos@gmail.com>
2024-03-20 20:05:43 -0300

    feat: adicionando fixture para categories_on_db.

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,11 +1,35 @@
 import pytest
 from app.db.connection import Session
+from app.db.models import Category as CategoryModel
 
 
-@pytest.fixture
+@pytest.fixture()
 def db_session():
     try:
         session = Session()
         yield session
     finally:
         session.close()
+
+
+@pytest.fixture()
+def categories_on_db(db_session):
+    categories = [
+        CategoryModel(name="Roupa", slug="roupa"),
+        CategoryModel(name="Carro", slug="carro"),
+        CategoryModel(name="Itens de cozinha", slug="itens-de-cozinha"),
+        CategoryModel(name="Decoracao", slug="decoracao"),
+    ]
+
+    for category in categories:
+        db_session.add(category)
+    db_session.commit()
+
+    for category in categories:
+        db_session.refresh(category)
+
+    yield categories
+
+    for category in categories:
+        db_session.delete(category)
+    db_session.commit()

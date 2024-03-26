24fabd298222ae879dcc5021a77d0132dd760bd7
Paulo Honorato <paulohhonoratos@gmail.com>
2024-03-25 22:23:46 -0300

    feat: adicionando teste para schema de output de produtos.

--- app/test/schemas/test_product_schema.py
+++ app/test/schemas/test_product_schema.py
@@ -1,5 +1,6 @@
 import pytest
-from app.schemas.product import Product, ProductInput
+from app.schemas.product import Product, ProductInput, ProductOutput
+from app.schemas.category import Category
 
 
 def test_product_schema():
@@ -74,5 +75,30 @@ def test_product_input_schema():
             "slug": "camisa-polo",
             "price": 22.99,
             "stock": 22,
+        }
+    }
+
+
+def test_product_output_schema():
+    category = Category(name="Roupa", slug="roupa")
+
+    product_output = ProductOutput(
+        id=1,
+        name="Camisa Polo",
+        slug="camisa-polo",
+        price=22.99,
+        stock=22,
+        category=category
+    )
+
+    assert product_output.dict() == {
+        "id": 1,
+        "name": "Camisa Polo",
+        "slug": "camisa-polo",
+        "price": 22.99,
+        "stock": 22,
+        "category": {
+            "name": "Roupa",
+            "slug": "roupa",
         }
     }
